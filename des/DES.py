87d0481731ae08a4934201abb9104577c650904b
vmel21 <34194825+vmel21@users.noreply.github.com>
2018-12-02 01:19:08 -0500

    error checking, bug fixes, byte array support

--- des/DES.py
+++ des/DES.py
@@ -3,6 +3,7 @@ DES cipher
 Takes in 64 bit key, message and encrypts
 """ 
 
+#initial permutation table
 IP = (
     58, 50, 42, 34, 26, 18, 10, 2,
     60, 52, 44, 36, 28, 20, 12, 4,
@@ -14,6 +15,7 @@ IP = (
     63, 55, 47, 39, 31, 23, 15, 7
 )
 
+#inverse of initial permutation (final permutation) table
 IP_INV = (
     40,  8, 48, 16, 56, 24, 64, 32,
     39,  7, 47, 15, 55, 23, 63, 31,
@@ -25,6 +27,7 @@ IP_INV = (
     33,  1, 41,  9, 49, 17, 57, 25
 )
 
+# removes 8 parity bits
 PC1 = (
     57, 49, 41, 33, 25, 17, 9,
     1,  58, 50, 42, 34, 26, 18,
@@ -36,6 +39,7 @@ PC1 = (
     21, 13, 5,  28, 20, 12, 4
 )
 
+# last step in creating 48 bit subkey
 PC2 = (
     14, 17, 11, 24, 1,  5,
     3,  28, 15, 6,  21, 10,
@@ -47,6 +51,7 @@ PC2 = (
     46, 42, 50, 36, 29, 32
 )
  
+ #expansion table
 E  = (
     32, 1,  2,  3,  4,  5,
     4,  5,  6,  7,  8,  9,
@@ -58,6 +63,7 @@ E  = (
     28, 29, 30, 31, 32, 1
 )
  
+# permutation box
 P = (
     16,  7, 20, 21,
     29, 12, 28, 17,
@@ -69,7 +75,8 @@ P = (
     22, 11, 4,  25
 )
 
-Sboxes = {
+# substitution box
+Sbox = {
     0: (
         14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
         0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
@@ -121,28 +128,71 @@ Sboxes = {
 }
  
 
+# Given bytearrays key and message enciphers the message using the key
+def encipher(key_bytes, message_bytes):
+    diff = 8 - len(key_bytes)
+    if (diff > 0):
+        #pad the byte array
+        byte = bytearray(8 - len(key_bytes))
+        key_bytes[:0] = byte
+    elif (diff < 0):
+        raise ValueError("You are NOT allowed to provide a KEY greater than 8 bytes long")
+    
+    msg_diff = 8 - len(message_bytes)
+    if (msg_diff > 0):
+        #pad byte array
+        byte = bytearray(8 - len(message_bytes))
+        message_bytes[8:] = byte
+    elif (msg_diff < 0):
+        raise ValueError("You are NOT allowed to provide a MESSAGE greater than 8 bytes long")
+    
+    key = int.from_bytes(key_bytes, byteorder='big', signed=False)
+    message = int.from_bytes(message_bytes, byteorder='big', signed=False)
+    cipher_text = encryption(key, message)
+    print("This is encrypted: {:x}".format(cipher_text))
+    result = bin(cipher_text)
+    # ****** use this to get back value for printing to GUI ==> print("{:x}".format(int.from_bytes(bytearray(int(result, 2).to_bytes((len(result) + 7) // 8, 'big')), byteorder='big', signed=False)))
+    return bytearray(int(result, 2).to_bytes((len(result) + 7) // 8, 'big')) #To display correctly , you need to format with {:x}
 
-def encryption(key, message, decryption = False):
-    print(len(key))
-    print(len(message))
+# Given bytearrays key and ciphertext deciphers the message using the key
+def decipher(key_bytes, ciphered_text_bytes):
+    diff = 8 - len(key_bytes)
+    if (diff > 0):
+        #pad the byte array
+        byte = bytearray(8 - len(key_bytes))
+        key_bytes[:0] = byte
+    elif (diff < 0):
+        #remove from right until 8 bytes 
+        key_bytes = key_bytes[:8]
+        
+    key = int.from_bytes(key_bytes, byteorder='big', signed=False)
+    ciphered_text = int.from_bytes(ciphered_text_bytes, byteorder='big', signed=False)
+    plain = encryption(key, ciphered_text, decryption=True)
+    print("This is decrypted {:x}".format(plain))
+    result = bin(plain)
+    #**** use this to get a printable value for GUI from bytearray ==> print("{:x}".format(int.from_bytes(bytearray(int(result, 2).to_bytes((len(result) + 7) // 8, 'big')), byteorder='big', signed=False)))
+    return bytearray(int(result, 2).to_bytes((len(result) + 7) // 8, 'big'))
+
+# Takes 64 bit key and message , encrypts message with key
+def encryption(key, message, decryption=False):
     assert isinstance(key, int) and isinstance(message, int)
-    assert key.bit_length == 64
-    assert  message.bit_length== 64
+    assert key.bit_length() <= 64
+    assert  message.bit_length() <= 64
 
-    key = permute_using_table(PC1, key, 64) #yields 56 bits
+    key = permute_using_table(PC1, key, 64) #yields 64 -> 56 bits
 
     #split key into 2 pieces
     C0 = key >> 28
-    D0 = key & (2 ** (28 - 1))
+    D0 = key & (2 ** 28 - 1)
 
     #create the 16 round keys
-    r_keys = make_round_keys(C0, D0) #yields 48 bit
+    r_keys = make_round_keys(C0, D0) #yields 56 -> 48 bit
 
     block_message = permute_using_table(IP, message, 64) 
-
+    
     #split plaintext block into 2 pieces
     L0 = block_message >> 32
-    R0 = block_message & (2 ** (32 - 1))
+    R0 = block_message & (2 ** 32 - 1)
 
 
     L_last = L0
@@ -152,23 +202,25 @@ def encryption(key, message, decryption = False):
     for i in range(1, 17):
         if decryption:
             i = 17 - i #apply the r_keys in reverse order
-        L_next = R_last
+
         R_next = L_last ^ round_function(R_last, r_keys[i])
-        L_last = L_next
+        L_next = R_last
+
         R_last = R_next
-
-
+        L_last = L_next
+       
     #rejoin the 2 halves
     ciphered_block = (R_next << 32) + L_next
-
+    
     #inverse original permutation
     ciphered_block = permute_using_table(IP_INV, ciphered_block, 64)
 
     return ciphered_block
 
+# Performs Feistal function on key and cipher block
 def round_function(Ri, Ki):
 
-    Ri = permute_using_table(E, Ri, 32) #yields 48 bits
+    Ri = permute_using_table(E, Ri, 32) #yields 32 -> 48 bits
 
     Ri = Ri ^ Ki # XOR operation
 
@@ -186,11 +238,12 @@ def round_function(Ri, Ki):
     for block, left_shift in Ri_blocks:
         Ri = Ri + (block << left_shift)
 
-    #permutation using P table
+    #permutation using P box
     Ri = permute_using_table(P, Ri, 32)
 
     return Ri
 
+# Given a block, and its length, performs a permutation dependent on the table
 def permute_using_table(table, block, block_length):
     string = bin(block)[2:].zfill(block_length)
     permutation = []
@@ -198,38 +251,26 @@ def permute_using_table(table, block, block_length):
     for spot in range(len(table)):
         permutation.append(string[table[spot] -1])
 
-    return int(''.join(perm), 2)
+    return int(''.join(permutation), 2)
 
+# helps perform shift on round keys during creation
 def left_rotating_func(val, right_bits, max_bits):
     return \
-    (val << r_bits%max_bits) & (2**max_bits-1) | \
-    ((val & (2**max_bits-1)) >> (max_bits-(r_bits%max_bits)))
+    (val << right_bits%max_bits) & (2**max_bits-1) | \
+    ((val & (2**max_bits-1)) >> (max_bits-(right_bits%max_bits)))
 
-def do_DES():
-    key = input("Please input the key = ")
-    message = input("Please input the message = ")
-
-    #NOTE: this somehow needs to turn them into 64 bit ints kill me
-    key = int(key).to_bytes(8, byteorder='big')
-    message = int(message).to_bytes(8, byteorder='big')
-    
-    ciphered = encryption(key, message)
-    print('encrypted: {}'.format(ciphered))
-
-    plain = encryption(key, message, decryption=True)
-    print('decrypted: {}'.format(plain))
-
+# creates the 16 subkeys necessary for DES 
 def make_round_keys(C0, D0):
     #need 16 keys
     r_keys = dict.fromkeys(range(0, 17))
     left_rotation = (1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1)
 
-    C0 = left_rotating_func(CO, 0, 28)
-    D0 = left_rotating_func(DO, 0, 28)
+    C0 = left_rotating_func(C0, 0, 28)
+    D0 = left_rotating_func(D0, 0, 28)
 
     r_keys[0] = (C0, D0)
 
-    #keep making round keys
+    #rotate each half-subkey by 1 or 2 bits in order to generate 48 bit subkey
     for i, rotation in enumerate(left_rotation):
         i = i + 1
         Ci = left_rotating_func(r_keys[i-1][0], rotation, 28)
@@ -238,13 +279,29 @@ def make_round_keys(C0, D0):
 
     del r_keys[0] #this is not one of the keys 
 
+    # combine left and right half-subkeys into full subkey
     for i, (Ci, Di) in r_keys.items():
         Ki = (Ci << 28) + Di
-        r_keys[i] = permute_using_table(PC2, Ki, 56) #yields 48 bits
+        r_keys[i] = permute_using_table(PC2, Ki, 56) #yields 56 -> 48 bits
 
     return r_keys
 
-do_DES()
+
+#********** FOR TESTING -------->
+#make sure everything starts with "0x"
+k = 0x8e6f4d63dea53de65f#0x281dc8a34d3f345d5d#0x0123456789ABCDEF #0x0e329232ea6d0d73 # 64 bit
+j = bin(k)
+ar = bytearray(int(j, 2).to_bytes((len(j) + 7) // 8, 'big'))
+
+
+k2 = 0x133457799BBCDFF1
+m = 0x8e6f4d63dea53de65f#0x281dc8a34d3f345d5d#0xf7c24f74fa2b167e#0x8787878787878787
+n = bin(m)
+mar = bytearray(int(n, 2).to_bytes((len(n) + 7) // 8, 'big'))
+
+if __name__ == '__main__':
+    enciphered = encipher(ar, mar)
+    decipher(ar, enciphered)
 
 
 
