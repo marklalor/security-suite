17eb15b4b239ae2af9a0ced14fcb5fb379af3d3f
Jacob Wise <jswise23@gmail.com>
2018-12-01 19:52:27 -0500

    fixed chunking
    TODO: ensure decrypt output is not padded

--- security-gui/src/edu/cwru/eecs444/fall2018/implementations/Rsa.java
+++ security-gui/src/edu/cwru/eecs444/fall2018/implementations/Rsa.java
@@ -10,6 +10,7 @@ import static edu.cwru.eecs444.fall2018.Utilities.bytesToHex;
 public class Rsa {
     private static final int DEFAULT_KEY_SIZE = 2048;
     private static final int DEFAULT_PUBLIC_EXP = 65537;
+    private static final int MAX_MESSAGE_LENGTH = DEFAULT_KEY_SIZE / 8;
     private static final BigInteger ONE = BigInteger.ONE;
     private static final BigInteger ZERO = BigInteger.ZERO;
 
@@ -56,19 +57,10 @@ public class Rsa {
      * @return byte[] This returns the encrypted message
      */
     private static byte[] encrypt(final byte[] m, final byte[] n, final byte[] e) {
-        byte[][] chunked;
+        byte[][] chunked = chunk(m, MAX_MESSAGE_LENGTH);
         byte[] encryptedChunk;
         int j = 0;
-
-        if(m.length < DEFAULT_KEY_SIZE){
-            chunked = new byte[1][m.length];
-            chunked[0] = m;
-        }
-        else{
-             chunked = chunk(m, DEFAULT_KEY_SIZE);
-        }
-
-        byte[] output = new byte[chunked.length * DEFAULT_KEY_SIZE];
+        byte[] output = new byte[MAX_MESSAGE_LENGTH * chunked.length];
 
         for(int i = 0; i < chunked.length; i++){
         	encryptedChunk = encrypt(new BigInteger(chunked[i]), new BigInteger(n), new BigInteger(e)).toByteArray();
@@ -91,7 +83,19 @@ public class Rsa {
      * @return byte[] This returns the decrypted message
      */
     private static byte[] decrypt(final byte[] c, final byte[] d, final byte[] n) {
-        return decrypt(new BigInteger(c), new BigInteger(d), new BigInteger(n)).toByteArray();
+        byte[][] chunked = chunk(c, MAX_MESSAGE_LENGTH);
+        byte[] decryptedChunk;
+        int j = 0;
+        byte[] output = new byte[MAX_MESSAGE_LENGTH * chunked.length];
+
+        for(int i = 0; i < chunked.length; i++){
+            decryptedChunk = decrypt(new BigInteger(chunked[i]), new BigInteger(d), new BigInteger(n)).toByteArray();
+            System.arraycopy(decryptedChunk, 0, output, j, decryptedChunk.length);
+            j = j + decryptedChunk.length;
+        }
+        return output;
+
+        //return decrypt(new BigInteger(c), new BigInteger(d), new BigInteger(n)).toByteArray();
     }
 
     private static BigInteger decrypt(final BigInteger c, final BigInteger d, final BigInteger n) {
@@ -128,15 +132,17 @@ public class Rsa {
     }
 
     private static byte[][] chunk(final byte[] input, int chunkLength) {
-        byte[][] chunkedInput = new byte[(input.length + 1) / chunkLength][chunkLength];
+        byte[][] chunkedInput = new byte[((input.length - 1) / chunkLength) + 1][];
 
         int i =0;
-        while (i < chunkedInput.length) {
+        while (i < chunkedInput.length - 1) {
             chunkedInput[i] = new byte[chunkLength];
-            System.arraycopy(input, chunkLength*i, chunkedInput[i], 0, Math.min(chunkLength, input.length-chunkLength*i));
+            System.arraycopy(input, chunkLength*i, chunkedInput[i], 0, chunkLength);
             i++;
         }
 
+        chunkedInput[i] = new byte[input.length-chunkLength*i];
+        System.arraycopy(input, chunkLength*i, chunkedInput[i], 0, input.length-chunkLength*i);
         return chunkedInput;
     }
 
@@ -149,7 +155,9 @@ public class Rsa {
         final byte[] privateKey = keyPair.getPrivateKey();
 
         final String message = "Hello world";
-        final byte[] messageBytes = message.getBytes();
+        final String message3000 = "30p4YyXIr3emYX3xkKKbRSudYelPnAoQAHNjdlOQGMV84XcgcuFkWamJERZbnH6HqOKgBWht6zNV4gxS4cNwT7Mjy0vEDyhoquT0Fv6Sps6MPS24Be20kjE5xYhtDStPpIHmbEydTbbL1UYb2NVfH1BFX4QLv4jeU8JpT2KW9TajOwZKZvwf3JXDWaNFDxOW37mvlNLj2ZqLil9GfyRXhGZRtR0kkBOvoEVTrNfvoMVfdOSi34RVmRUs7clxF5FRcseH5ReISikaDrU9EsXtmiK56kIj2ZSroEWxhCSuzTo1PI1gU52cEqOdKTkeOHOxWRLyu1LSwghxVcgFvAQpjvMIuuL9pojvgNNrMdIO6OIUUlccNCPOczCKw06hvnlqnTkKMIihHwc2ty6ganivVcRr69VHGHQZ78Q4DAUtkBN8NMTh9WchHDwindKeg4ADKnOq4xGBI6h86M0Bn7Pi08wPUZtG0smTOvfYNsrqqAJu6Zrsr8y9O7I1FHQBgh5UxRIVeuFuqor541sY60TBrOeN9ouZjsWvvxkHd6LOR0fGjyYOmgvMUJQAgkGLSJXsGilLilNGdAhAqLRp6N7qCfqUrSobFaQeNWMmE0M1HZpTcv5ILnCbzIVwzUUQAL83r9XQLfGDqZCN6V4SDqoiuvJzLPntZDBsQ0rYEZStS3gb8EAqwRN0SjKfiHDA9OUhlgffCTUCP1of8bhN6n2M9cxKARW06IYoOrXFOxMwKOMVOx8LbzFRjWiPMlQQx6KGbeiA23B6ymuJ6PaHBf5lUB67lF7FLwEiMYSzkZPhlhQSqmftsfnlQpgNhScWciSKprIjV2KN6nmCGRIqjc4X9kEzjJvVHQPm8fGJDsh5JfEtMQhbpBbK2TYh3HrFZ5rcp2qMusnhEJATIhyclbCELxPeE9ZM6ItnQ1lCVSXDk7O1uVNjaBTwI31fvfnYdkiFs8z4nFagLurhcd3rQc7b9WQKgdRiuxWXKyxFL4Gb0ojPWPSsuOvYSHRYjeBM9mtyL0a6KhiH04yehcqRrfeOSDLtYTNrF6SxUGabCjT2ytRYiTEx1f00ZcoLxa00ExFbhu8zp5bd9t26qxmWuz7wPdAefqsQyCG0lTsfzaDsg5Fadw7Vtyl9JMqUclZbeNZeHNZOOUKPnpRJOTl7zjyhz7PURqujXJ6OJ56S6cfrp4ounsMOxnZxqt62sEuixKGOUSa72iRQj8sGpjRV8lmquaDQoqpqOLbsWi3oWbzc9vRblioLz9QiV1on4xbqkqTvO7SXZ0mKfUQ5e03PS0Ml6rBKCCxYtSwkUiWr4WGDpBofrlx7CVHUT7jRrjB5GWSD6q4GdpXsLoJ5aSyOkP1ODutHN4TudRcFF3brUFeRsxpQTTEH0IqvTGmcYEiKnU9j1jAOqXdpFpmqhwCpbu4seyMvmagHbTWJpxjA7yUUNw1941Cqc0BrxqYHZPDNsDf2p391FNxdcFYXEX8xBO4ZG0SgNud5O1r5kEXITndRhkOXzRJOVbvXfqHYNyZNrunUFOPBPZ41YGVSI3s5uuT3K5bhhLldZ2vw20KdfnIN2Quhf8elglOyWb9wJew2u6whh0TgbqiLZO58VCclt3fSSQTnzlDcAWEXD8SR9xUlvOOG3QbTHDWYnhUX9KdcWy6zrjaZp2KytW8JSEF0hZ1noNm61AKTByQvpwUXRVZU0wrDmiJlF1GQogKZnC9c5ADF74PylINc4J9XNZpT6I6VW0EIE0CEAxJrjEioYFfTQrKyS4feYL2mAO3onctWIvCpEZlyi6i6Z7Uh8z4mXHxO4IBlMoE6F6UmFw2ssDWStCGSdSzcOhWwvRFaROV9AOUkwuuS9ILVxmlPjTju9lfr9Rkm0TpzpWZmn4TWPyW0yxRIjw5OnTCiQuPRZvFhDMSSLhOtSnyo6PqVVIwzl4oxu7r0fHRLFXK2UqPzBBCgHpEJzIpxG9f96TcPn1ldx5gdJafKuUnY0H2ZDX0lLoDJ9pCHGnwtInGgntCTF4LN2FouU2FIVwunMUdmzVQXACi7OX2zwzn7vTylRycZRmc9IptLtjOmuTe8gswe8iZ7ULMgrwoz2iJR2hEl7daPMMjwqJTGWzgKVNqukIXq59UZnwToZI3oVOw2NtYIKXNCa9draPVWoQa0XpgNXWaIJQcfcTcNANFx17JUPNOHNRDnRNHxGwnKK5ZwcJBlriwH3BMtXoSG6TkNGr8NnG6CAdHbAhUrjXwnQWgVHsQkbEfKIPRhpo7CUzgOqlLVUGkxgtqbfBE0zPgJIyLWGQJ7gQXKqUOZ8qfucYc191QOwyTjqDArZGQDKD0SVjZm90a2wB1bjK2Ol9rnIsDvx2n6ZUtB7gdlkjEm2aBeMXL93R0GHEZTQVAiz3GBNrKZl9XA1ecHdm7wMErhimLlgyMimRwAQaID4GwdRKf0EwT5YbvKd3pBRkS3VhLKN6j8qqfrfTrgXh2aKgQlyCPALGkBYZtl5620o6YU7apXgCSzDj9U0Ab1zMDlYhy1Y1Und67tvsNlawK6l56eQe344tnY5CtqeaVJX450Ir56TYz0jEfV7V3vtTXIHUeRYKMYNMsERC7aXf41sHXdAa8LX7rWI8pREreBgOCyLeV4yCWD2bgg70EL7G6EJyAHkXDwoZzvHw0TNtSPtcgFrpiqsBfys2bhYbH6cPoODmEZtEMwHNmVbhddtJqhYUOB8KYzMuecWhmEvym9jfuxQdjn01yenmmx6bf2dPTE67vqyfyhO1qDutLpN1FMvl3p3edmoSRYuB3P7CBgWaHoOWwOIKN0gwMiIzcA80LUWV9g4XluRvrNAOgkk3rUWcwmj9SROLbkMxJKgkt9R3ci0xeOBPcNtVkv1XBcOJhZ8GD3FU0eGpylLHzXaywhq1zN1pPNYf0WpHzwA4Ds15RaRLldAgXnT3A07vNqXeQ9QOhrQ7fNxb1a0pMCUVh84CsCLohnwlVHMAm32G2AFNWdseKe";
+        final String messageOther = "2gZkajleugKwUsbtKwfFpBNHyup0poaggOMV9jXvX8F08o7rTyTh8KX1Nqf3bkh9I5kMSIv6CM90juVIrzsjhOA3ZVpB0OYYoCwngjy2VmXNTJyVOlMXutAGfQ3HGpjVs0bypznb9ynbIQemcVIKAOlbxLOVoWAmVPqTOEz5ZqnokzbHl0y6PWOBNTHeZLSNeN6ggHGvSOtsu8qmUB1n4l7WObvvobB10Mbv4I2xDjgYWED5KK6oQQfYwHlG1ich";
+        final byte[] messageBytes = messageOther.getBytes();
         System.out.printf("Original bytes: %s\n", bytesToHex(messageBytes));
         final byte[] encryptedBytes = encrypt(messageBytes, publicKey, publicKeyExp);
         System.out.printf("Encrypted bytes: %s\n", bytesToHex(encryptedBytes));
